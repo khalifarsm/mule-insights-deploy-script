798c567ed995f64dcae858f81705381dc2fcf34f
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2023-04-18 10:13:33 +0000

    initial commit

---
+++ dockerserver.py
@@ -0,0 +1,30 @@
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _send_response(self, message):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(message, "utf8"))
+
+    def do_POST(self):
+        if self.path == "/docker/refresh":
+            command = "docker pull khalifarsm/analyser"
+            output1 = os.popen(command).read()
+            print(output1)
+            command = "docker pull khalifarsm/analyserui"
+            output2 = os.popen(command).read()
+            print(output2)
+            command = "docker stack deploy -c /home/ec2-user/docker-compose.yml analyser"
+            output = os.popen(command).read()
+            print(output)
+            command = "docker system prune -af"
+            output3 = os.popen(command).read()
+            print(output3)
+            self._send_response(output)
+        else:
+            self._send_response("Invalid path")
+
+httpd = HTTPServer(("", 8085), RequestHandler)
+httpd.serve_forever()
