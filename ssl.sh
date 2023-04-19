5bda4434ae2689fe0b5d26c520c71145067a5356
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2023-04-19 10:59:16 +0000

    do not stop nginx

--- ssl.sh
+++ ssl.sh
@@ -1,4 +1,4 @@
 systemctl stop nginx
 certbot --nginx -d muleinsights.com -d www.muleinsights.com -d analyze.muleinsights.com
-cp nginx-ssh.conf /etc/nginx/nginx.conf
+cp nginx-ssl.conf /etc/nginx/nginx.conf
 systemctl start nginx
