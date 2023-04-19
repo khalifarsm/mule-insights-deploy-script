d7a4b3c32b982b7d66f827d2554ae5b717cdfae5
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2023-04-19 11:01:09 +0000

    add ssl script

--- ssl.sh
+++ ssl.sh
@@ -1,4 +1,3 @@
-systemctl stop nginx
 certbot --nginx -d muleinsights.com -d www.muleinsights.com -d analyze.muleinsights.com
 cp nginx-ssl.conf /etc/nginx/nginx.conf
-systemctl start nginx
+systemctl restart nginx
