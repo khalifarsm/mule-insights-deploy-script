3ad5006941851f811097f1f1af40839abcc7b89e
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2023-04-25 10:30:34 +0100

    separate backend from front-end

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,3 @@
-certbot --nginx -d muleinsights.com -d www.muleinsights.com -d analyze.muleinsights.com
+certbot --nginx -d api.muleinsights.com -d analyze.muleinsights.com
 cp nginx-ssl.conf /etc/nginx/nginx.conf
 systemctl restart nginx
